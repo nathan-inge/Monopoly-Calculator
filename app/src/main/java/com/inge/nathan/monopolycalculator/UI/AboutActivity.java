111a1103282b64a4469f01732b86610997b0712a
ninge <nate8983@gmail.com>
2018-03-31 18:27:07 -0600

    Working on in app billing

--- app/src/main/java/com/inge/nathan/monopolycalculator/UI/AboutActivity.java
+++ app/src/main/java/com/inge/nathan/monopolycalculator/UI/AboutActivity.java
@@ -1,18 +1,26 @@
 package com.inge.nathan.monopolycalculator.UI;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.vending.billing.IInAppBillingService;
+import com.inge.nathan.monopolycalculator.InAppBillingUtil.IabHelper;
+import com.inge.nathan.monopolycalculator.InAppBillingUtil.IabResult;
 import com.inge.nathan.monopolycalculator.R;
 
 public class AboutActivity extends AppCompatActivity {
@@ -21,6 +29,11 @@ public class AboutActivity extends AppCompatActivity {
     private TextView activityTitle;
     private ImageButton backButton;
     private Button sendFeedbackButton;
+
+    private IabHelper mHelper;
+
+    private IInAppBillingService mService;
+    private ServiceConnection mServiceConn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +55,32 @@ public class AboutActivity extends AppCompatActivity {
             versionText.setText(version);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
+        }
+
+        mServiceConn = new ServiceConnection() {
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mService = null;
+            }
+
+            @Override
+            public void onServiceConnected(ComponentName name,
+                                           IBinder service) {
+                mService = IInAppBillingService.Stub.asInterface(service);
+            }
+        };
+
+        Intent serviceIntent =
+            new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mService != null) {
+            unbindService(mServiceConn);
         }
     }
 
